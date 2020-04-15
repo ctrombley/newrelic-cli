709ebe891ff3dadcad196b5006b071c7e8296712
Chris Trombley <ctrombley@newrelic.com>
2020-04-15 16:56:38 -0700

    fix script

--- build/document.mk
+++ build/document.mk
@@ -28,5 +28,8 @@ release-notes: tools
 	@echo "=== $(PROJECT_NAME) === [ release-notes    ]: Generating release notes..."
 	@mkdir -p $(SRCDIR)/tmp
 	@$(CHANGELOG_CMD) --silent -o $(SRCDIR)/tmp/$(RELEASE_NOTES_FILE) v$(PROJECT_VER_TAGGED)
+	@ls $(SRCDIR) 
+	@echo
+	@ls $(SRCDIR)/tmp
 
 .PHONY: docs changelog release-notes
